744d5455bf5349418a7200f5303d461e8a6e605d
Jacob Kittley-Davies <jacob@kittley.com>
2018-03-04 14:05:54 +0000

    Added remote host details for backup upload

--- server/config/secure.example.py
+++ server/config/secure.example.py
@@ -10,3 +10,8 @@ SECURITY_PASSWORD_SALT = "jdsfhbasdjkfhasldjkfhasiuy324783y58934"
 
 DEFAULT_USER = "default@default.none"
 DEFAULT_PASS = "password"
+
+BACKUP_REMOTE_HOST = '192.168.1.188' 
+BACKUP_REMOTE_USER = 'raspberry'
+BACKUP_REMOTE_PASS = 'pi'
+BACKUP_REMOTE_DIR  = 'srv/test_backup'
