c8e0525d474d195de2121731c54475319e125ecf
Jacob Kittley-Davies <jacob@kittley.com>
2018-03-19 15:51:43 +0000

    New classes added to package

--- server/utils/__init__.py
+++ server/utils/__init__.py
@@ -1,4 +1,6 @@
 from .devices import Device, Devices
+from .metric import Metric
+from .readings import Readings
 from .logger import Logger
 from .backup import BackupManager
 from .influx import get_InfluxDB
