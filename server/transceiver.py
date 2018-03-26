c6bd8c1530f3941ccbb3aeabad2cc7c4b9072f28
Jacob Kittley-Davies <jacob@kittley.com>
2018-03-26 21:29:51 +0100

    Removed old comments and added add readings code

--- server/transceiver.py
+++ server/transceiver.py
@@ -8,7 +8,7 @@ import serial_asyncio
 from serial.serialutil import SerialException
 import signal
 import click, arrow
-from utils import SerialLogger, SerialTXQueue, Logger
+from utils import SerialLogger, SerialTXQueue, Logger, Device
 
 loop = None
 log = Logger()
@@ -43,6 +43,19 @@ class Output(asyncio.Protocol):
         self.json_str += data.decode("utf-8")
         if b'\n' in data:
             serial_log.rx(str(self.json_str))
+            try:
+                asjson = json.loads(str(self.json_str))
+                utc = "NOW" if 'utc' not in asjson else asjson['utc']
+                field_id = "{}_{}_{}".format(asjson['dtype'], asjson['field'], asjson['unit'])
+                fields = dict()
+                fields[field_id] = asjson['value']
+                device = Device(asjson['device_id'], True)
+                device.add_reading(utc, fields, 'serial_port')
+            except KeyError as e:
+                log.error('Serial data missing key', exception=str(e))
+            except Exception as e:
+                log.error('Serial receive failed', exception=str(e))
+                
             self.json_str = ""
             # self.transport.close()
 
@@ -77,34 +90,6 @@ def launch(test=False, verbose=False):
     loop.run_forever()
     loop.close()
 
-
-
-    # if test:
-    #     from commlink.test import ManagerTEST
-    #     comm = ManagerTEST(pack_formatter_test, save_reading, verbose=True)
-    #     log.debug("Radio TEST Starting...")
-    # else:
-    #     # Initialise
-    #     from commlink.rfm69 import ManagerRFM69
-    #     comm = ManagerRFM69(pack_formatter_rf69, save_reading, verbose=verbose)
-    #     log.debug("Radio RFM69 Starting...")
-    
-    # # Add radio to pool
-    # comms_pool.append(comm)
-    # comm.initialise()
-    
-    # # comm.transmit(2, "Hi ya")
-
-    # # This loop tries repeatedly to make receive work.
-    # while True:
-    #     try:
-    #         comm.receive()
-    #     except Exception as e:
-    #         log.error("BG Service Receiving...", error=str(e))
-    #         print (e)
-        
-
-
 # # =============================================================================
 # # Do not edit below this line
 # # =============================================================================
@@ -119,57 +104,3 @@ if __name__ == "__main__":
     signal.signal(signal.SIGTERM, signal_handler)
     signal.signal(signal.SIGINT, signal_handler)
     launch()
-
-
-
-
-# import signal, sys, string, time
-# from random import randint, choice
-# import click, arrow
-# from utils import Logger, Device
-# from utils.exceptions import InvalidPacket
-
-# log = Logger()
-# comms_pool = []
-
-# # =============================================================================
-# # Formatter functions. Each commlink class takes a formatter function when
-# # instansiated. The purpose of the function is to format the received packet
-# # data into a dictionary of field:value pairs which will be saved to the 
-# # database. Each class will send different data in a dict called 'unformatted'
-# # you must see the instructions for the radio class you are using. Every 
-# # formatter must return three thing: timestamp (utc), device_id (sting) and a 
-# # dictionary of field:value pairs as described above.
-# # =============================================================================
-# # Little Sense does it’s best to minimise configuration server side. To help 
-# # this, all sensor devices should transmit readings where the variable name 
-# # is constructed as `<dtype>_<variable-name>_<unit>`.
-# # 
-# # - dtype: (Data type) can be either int, float, bool, str or string. 
-# # - name:  The name can include extra semicolons e.g. float_light_level_lux is 
-# #          valid. 
-# # - unit:  Unit can be anything you want, but is used to automatically group 
-# #          reading in some visualisations.
-# # =============================================================================
-
-
-# # =============================================================================
-# # Function to act on the formatted packets (returned by the punction above)
-# # =============================================================================
-
-# def save_reading(commlink_name, utc, device_id, sensor_readings):
-#     dev = Device(device_id, True)
-#     dev.add_reading(utc, sensor_readings, commlink_name)
-#     return
-
-
-
-
-
-
-
-
-
-
-
-    
