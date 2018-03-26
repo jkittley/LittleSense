0140503fe6d43054ea0664f35a163b1de5c9d065
Jacob Kittley-Davies <jacob@kittley.com>
2018-03-26 21:26:55 +0100

    More sensor data and matching dashboard

--- device/arduino/serial_test/serial_test.ino
+++ device/arduino/serial_test/serial_test.ino
@@ -3,7 +3,7 @@
 // 
 #include <ArduinoJson.h>
 
-String device_id = "test_device_99";
+String device_id = "test_device_1";
 // ========================
 // Setup
 // ========================
@@ -20,18 +20,24 @@ void setup() {
 // ========================
 void loop() {  
   
+  sendReading("float", "signal", "db", random(0,450)/10);
+  sendReading("int", "light_level", "lux", random(300,500)/10);
+  sendReading("bool", "switch", "state", random(0,2));
+  
+  delay(3000);
+}
+
+void sendReading(String dtype, String field_name, String unit, int value) {
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
-  
   root["device_id"] = device_id;
-  root["dtype"] = "float";
-  root["field"] = "temperature";
-  root["unit"] = "C";
-  root["value"] = random(0,450) / 10;
+  root["dtype"] = dtype;
+  root["field"] = field_name;
+  root["unit"] = unit;
+  root["value"] = value;
   root["utc"] = "NOW";
-  
   root.printTo(Serial);
   Serial.println();
-  delay(3000);
+  delay(100);
 }
 
