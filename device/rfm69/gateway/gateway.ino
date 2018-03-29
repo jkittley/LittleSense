4f505cd01c1acba5234aabd8f0b429d9f1ccbc26
Jacob Kittley-Davies <jacob@kittley.com>
2018-03-29 19:23:34 +0100

    Updated code with more support for Wings

--- device/rfm69/gateway/gateway.ino
+++ device/rfm69/gateway/gateway.ino
@@ -8,6 +8,9 @@
 // this script has an example. In addition, the script translates serial messages set 
 // from the server and transmits them to devices.
 //
+// Note: Because it is expected that the serial output is going to be read by the 
+//       server. Start all messages with a # and they will be ignored.
+//
 // **********************************************************************************
 
 #include <RFM69.h>         //get it here: https://www.github.com/lowpowerlab/rfm69
@@ -18,6 +21,9 @@
 // Node and network config
 #define NODEID        1    // The ID of this node (must be different for every node on network)
 #define NETWORKID     100  // The network ID
+
+// Are you using the RFM69 Wing? Uncomment if you are.
+#define USING_RFM69_WING 
 
 // The transmision frequency of the baord. Change as needed.
 #define FREQUENCY      RF69_433MHZ //RF69_868MHZ // RF69_915MHZ
@@ -37,18 +43,29 @@
 // Which LED
 #define LED LED_BUILTIN
 
-// Board specific config - You should not need to edit
-#if defined (__AVR_ATmega32U4__)
-  #define RF69_RESET    4
-  #define RF69_SPI_CS   8
-  #define RF69_IRQ_PIN  7
-  #define RF69_IRQ_NUM  4
+// Board and radio specific config - You should not need to edit
+#if defined (__AVR_ATmega32U4__) && defined (USING_RFM69_WING)
+    #define RF69_SPI_CS  10   
+    #define RF69_RESET   11   
+    #define RF69_IRQ_PIN 2 
+    #define RF69_IRQ_NUM digitalPinToInterrupt(RF69_IRQ_PIN) 
+#elif defined (__AVR_ATmega32U4__)
+    #define RF69_RESET    4
+    #define RF69_SPI_CS   8
+    #define RF69_IRQ_PIN  7
+    #define RF69_IRQ_NUM  4
+#elif defined(ARDUINO_SAMD_FEATHER_M0) && defined (USING_RFM69_WING)
+    #define RF69_RESET    11
+    #define RF69_SPI_CS   10
+    #define RF69_IRQ_PIN  6
+    #define RF69_IRQ_NUM  digitalPinToInterrupt(RF69_IRQ_PIN)
 #elif defined(ARDUINO_SAMD_FEATHER_M0)
-  #define RF69_RESET    4
-  #define RF69_SPI_CS   8
-  #define RF69_IRQ_PIN  3
-  #define RF69_IRQ_NUM  3
+    #define RF69_RESET    4
+    #define RF69_SPI_CS   8
+    #define RF69_IRQ_PIN  3
+    #define RF69_IRQ_NUM  3
 #endif
+
 
 // Create Radio
 #ifdef ENABLE_ATC
@@ -86,15 +103,14 @@ void setup() {
   printDebugInfo();
 }
 
-
 // Define payload
 typedef struct {
-  char msg[50]; // A string msg
-  uint8_t uptime; //uptime in ms
-  uint8_t temp;   //temperature
+  char command[20]; // command
+  uint8_t battery; // Battery voltage
+  uint8_t volume;   // Volume
+  uint8_t rssi;     // RSSI as seen by node
 } Payload;
 Payload payload;
-
 
 // Main loop
 void loop() {
@@ -102,14 +118,16 @@ void loop() {
   // Process any serial input
   if (Serial.available() > 0) {
     
-    char input = Serial.read();
+    String input = Serial.readString();
     if (input == 'd') {
       printDebugInfo();
     }
 
-    if (isDigit(input)) {
+    if (input.toInt() > 0) {
       char buff[] = "ShowYourSelf";
-      Serial.print("Asking Node "); Serial.print(String(input).toInt()); Serial.println(" to show itself");
+      
+      Serial.print("# Asking Node "); Serial.print(String(input).toInt()); Serial.println(" to show itself");
+      
       if (radio.sendWithRetry(String(input).toInt(), buff, sizeof(buff))) {
         Serial.println(" Acknoledgment received!");
       } else {
@@ -121,28 +139,26 @@ void loop() {
   // Process packets received
   if (radio.receiveDone()) {
     
-    Serial.print('[sender: '); Serial.print(radio.SENDERID, DEC); Serial.println("] ");
-    if (promiscuousMode) { Serial.print("to [");Serial.print(radio.TARGETID, DEC);Serial.print("] "); }
-
-    Serial.print("Data length: "); Serial.println(radio.DATALEN);
-    Serial.print("[RX_RSSI:"); Serial.print(radio.RSSI); Serial.print("]");
+    Serial.print("# sender: "); Serial.println(radio.SENDERID, DEC);
+    Serial.print("# target  "); Serial.println(radio.TARGETID, DEC);
+    Serial.print("# Data length: "); Serial.println(radio.DATALEN);
+    Serial.print("# RX_RSSI:"); Serial.println(radio.RSSI); 
 
     if (radio.DATALEN != sizeof(Payload)) {
-      Serial.println("Invalid payload received, not matching Payload struct!");
+      Serial.print("# Invalid payload received, not matching Payload struct. -- ");
       Serial.print(radio.DATALEN);
       Serial.print(" vs ");
       Serial.println(sizeof(Payload));
     } else {    
       payload = *(Payload*)radio.DATA; //assume radio.DATA actually contains our struct and not something else
-      Serial.print(" msg=");
-      Serial.print(payload.msg);
-      Serial.print(" uptime=");
-      Serial.print(payload.uptime);
-      Serial.print(" temp=");
-      Serial.print(payload.temp);
+      
+      sendReadingToSerial(radio.SENDERID, "float", "battery_level", "volts", payload.battery);
+      sendReadingToSerial(radio.SENDERID, "float", "signal_strength", "db", radio.RSSI);
+      sendReadingToSerial(radio.SENDERID, "float", "audio_level", "db", payload.volume);
+      
       if (radio.ACKRequested()) {
         radio.sendACK();
-        Serial.print(" - ACK sent.");
+        Serial.print("# ACK sent.");
       }
     }
         
@@ -150,6 +166,7 @@ void loop() {
     Blink(3, 1);
   }
 }
+
 
 // Helper function to blink the LED
 void Blink(int delayms, int numberTimes) {
@@ -163,7 +180,7 @@ void Blink(int delayms, int numberTimes) {
 
 // Reset the Radio
 void resetRadio() {
-  Serial.print("Resetting radio...");
+  Serial.print("# Resetting radio...");
   pinMode(RF69_RESET, OUTPUT);
   digitalWrite(RF69_RESET, HIGH);
   delay(20);
@@ -175,36 +192,46 @@ void resetRadio() {
 // Print Info
 void printDebugInfo() {
   char buff[50];
-  sprintf(buff, "\nListening at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
-  Serial.println(buff);
-  Serial.print("RF69_SPI_CS: "); Serial.println(RF69_SPI_CS);
-  Serial.print("RF69_IRQ_PIN: "); Serial.println(RF69_IRQ_PIN);
-  Serial.print("RF69_IRQ_NUM: "); Serial.println(RF69_IRQ_NUM);
-  #ifdef ENABLE_ATC
-    Serial.println("RFM69 Auto Transmission Control: Enabled");
+  #if defined (__AVR_ATmega32U4__)
+    Serial.println("# AVR ATmega 32U4");
   #else
-    Serial.println("RFM69 Auto Transmission Control: Disabled");
+    Serial.println("# SAMD FEATHER M0");
+  #endif
+  #ifdef USING_RFM69_WING
+    Serial.println("# Using RFM69 Wing: YES");
+  #else
+    Serial.println("U# sing RFM69 Wing: NO");
+  #endif
+  Serial.print("# RF69_SPI_CS: "); Serial.println(RF69_SPI_CS);
+  Serial.print("# RF69_IRQ_PIN: "); Serial.println(RF69_IRQ_PIN);
+  Serial.print("# RF69_IRQ_NUM: "); Serial.println(RF69_IRQ_NUM);
+  #ifdef ENABLE_ATC
+    Serial.println("# RFM69 Auto Transmission Control: Enabled");
+  #else
+    Serial.println("# RFM69 Auto Transmission Control: Disabled");
   #endif
   if (promiscuousMode) {
-    Serial.println("Promiscuous Mode: Enabled");
+    Serial.println("# Promiscuous Mode: Enabled");
   } else {
-    Serial.println("Promiscuous Mode: Disabled");
+    Serial.println("# Promiscuous Mode: Disabled");
   }
   #ifdef ENCRYPTKEY
-    Serial.println("Encryption: Enabled");
+    Serial.println("# Encryption: Enabled");
   #else
-    Serial.println("Encryption: Disabled");
+    Serial.println("# Encryption: Disabled");
   #endif
+  sprintf(buff, "# Listening at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  Serial.println(buff);
 }
 
 // sendReadingToSerial
-void sendReadingToSerial(String dtype, String field_name, String unit, float value) {
+void sendReadingToSerial(int sender_id, String dtype, String field_name, String unit, float value) {
   // Build message
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
-  root["device_id"] = "device_"+String(NODEID);
+  root["device_id"] = "test_device_"+String(sender_id);
   root["dtype"] = dtype;
-  root["field"] = field_name;
+  root["name"] = field_name;
   root["unit"] = unit;
   root["value"] = value;
   root["utc"] = "NOW";
@@ -213,6 +240,7 @@ void sendReadingToSerial(String dtype, String field_name, String unit, float val
   // Print to serial
   root.printTo(Serial);
   Serial.println();
+  delay(100);
 }
 
 // Identify yourself
@@ -223,7 +251,6 @@ void showMe() {
   Blink(100, 10);
   Blink(500, 2);
 }
-
 
 
 
--- device/rfm69/node/node.ino
+++ device/rfm69/node/node.ino
@@ -4,16 +4,20 @@
 //                                                       
 // **********************************************************************************
 
-#include <RFM69.h>         //https://www.github.com/lowpowerlab/rfm69
-#include <RFM69_ATC.h>     //https://www.github.com/lowpowerlab/rfm69
-#include <SPI.h>           //Included with Arduino IDE
-#include <ArduinoJson.h>   //https://arduinojson.org/d
+#include <RFM69.h>              // https://www.github.com/lowpowerlab/rfm69
+#include <RFM69_ATC.h>          // https://www.github.com/lowpowerlab/rfm69
+#include <SPI.h>                // Included with Arduino IDE
+#include <ArduinoJson.h>        // https://arduinojson.org/d
+#include <Adafruit_SleepyDog.h> // https://github.com/adafruit/Adafruit_SleepyDog
 
 // Node and network config
-#define NODEID        2    // The ID of this node (must be different for every node on network)
+#define NODEID        3    // The ID of this node (must be different for every node on network)
 #define NETWORKID     100  // The network ID
 #define GATEWAYID     1    // Where to send data
 int TRANSMITPERIOD = 5000; // Transmission interval in ms e.g. 5000 = every 5 seconds 
+
+// Are you using the RFM69 Wing? Uncomment if you are.
+//#define USING_RFM69_WING 
 
 // The transmision frequency of the baord. Change as needed.
 #define FREQUENCY      RF69_433MHZ //RF69_868MHZ // RF69_915MHZ
@@ -33,17 +37,34 @@ int TRANSMITPERIOD = 5000; // Transmission interval in ms e.g. 5000 = every 5 se
 // Which LED
 #define LED LED_BUILTIN
 
-// Board specific config - You should not need to edit
-#if defined (__AVR_ATmega32U4__)
-  #define RF69_RESET    4
-  #define RF69_SPI_CS   8
-  #define RF69_IRQ_PIN  7
-  #define RF69_IRQ_NUM  4
+// Board and radio specific config - You should not need to edit
+#if defined (__AVR_ATmega32U4__) && defined (USING_RFM69_WING)
+    #define RF69_SPI_CS  10   
+    #define RF69_RESET   11   
+    #define RF69_IRQ_PIN 2 
+    #define RF69_IRQ_NUM digitalPinToInterrupt(RF69_IRQ_PIN) 
+#elif defined (__AVR_ATmega32U4__)
+    #define RF69_RESET    4
+    #define RF69_SPI_CS   8
+    #define RF69_IRQ_PIN  7
+    #define RF69_IRQ_NUM  4
+#elif defined(ARDUINO_SAMD_FEATHER_M0) && defined (USING_RFM69_WING)
+    #define RF69_RESET    11
+    #define RF69_SPI_CS   10
+    #define RF69_IRQ_PIN  6
+    #define RF69_IRQ_NUM  digitalPinToInterrupt(RF69_IRQ_PIN)
 #elif defined(ARDUINO_SAMD_FEATHER_M0)
-  #define RF69_RESET    4
-  #define RF69_SPI_CS   8
-  #define RF69_IRQ_PIN  3
-  #define RF69_IRQ_NUM  3
+    #define RF69_RESET    4
+    #define RF69_SPI_CS   8
+    #define RF69_IRQ_PIN  3
+    #define RF69_IRQ_NUM  3
+#endif
+
+// Battery pin
+#if defined (__AVR_ATmega32U4__) 
+  #define VBATPIN A9
+#elif defined(ARDUINO_SAMD_FEATHER_M0)
+  #define VBATPIN A7
 #endif
 
 // Create Radio
@@ -83,7 +104,7 @@ long lastPeriod = 0;
 void loop() {
 
   // Send a reading
-  sendReading("float", "signal", "db", random(0,450)/10);
+  sendReading();
   delay(5);
 
   // Listen form messages for x milliseconds before going to sleep
@@ -101,48 +122,62 @@ void loop() {
   }
 
   Serial.println("Going to sleep");
-  delay(TRANSMITPERIOD);
+  radio.sleep();
+  Watchdog.sleep(TRANSMITPERIOD);
 }
 
 // Define payload
 typedef struct {
-  char msg[50]; // A string msg
-  uint8_t uptime; //uptime in ms
-  uint8_t temp;   //temperature
+  char command[20]; // command
+  uint8_t battery; // Battery voltage
+  uint8_t volume;   // Volume
+  uint8_t rssi;     // RSSI as seen by node
 } Payload;
 Payload payload;
 
+
+
 // sendReading
-void sendReading(String dtype, String field_name, String unit, float value) {
-  // Set payload data
-  String("Hello").toCharArray(payload.msg, sizeof(payload.msg));
-  payload.uptime = millis();
-  payload.temp   = 91.23; 
+void sendReading() {
+  
+  // Set payload command
+  String("none").toCharArray(payload.command, sizeof(payload.command));
+
+  // Battery Level
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024; // convert to voltage
+  payload.battery = 100 * measuredvbat;
+
+  // RSSI
+  payload.rssi    = radio.RSSI * 100;
+
+  // Volume
+  payload.volume  = random(0,450)/10; 
   
   // Print payload
-  printPayload();
+  Serial.print("Sending payload (");
+  Serial.print(sizeof(payload));
+  Serial.print(" bytes)");
+  Serial.print(" battery=");
+  Serial.print(payload.battery);
+  Serial.print(" signal=");
+  Serial.print(payload.rssi);
+  Serial.print(" volume=");
+  Serial.print(payload.volume);
+  Serial.println(")");
+  
   // Send payload
   if (radio.sendWithRetry(GATEWAYID, (const void*) &payload, sizeof(payload))) {
     Serial.println(" Acknoledgment received!");
+    Blink(100, 1);
   } else {
     Serial.println(" No Acknoledgment after retries");
+    Blink(500, 1);
   }
-  Blink(100, 1);
 }
 
-// Print out the packet for debug
-void printPayload() {
-  Serial.print("Sending payload struct (");
-  Serial.print(sizeof(payload));
-  Serial.print(" bytes)");
-  Serial.print(" msg=");
-  Serial.print(payload.msg);
-  Serial.print(" uptime=");
-  Serial.print(payload.uptime);
-  Serial.print(" temp=");
-  Serial.print(payload.temp);
-  Serial.println("--");
-}
 
 // Helper function to blink the LED
 void Blink(int delayms, int numberTimes) {
@@ -166,11 +201,20 @@ void resetRadio() {
   Serial.println(" complete");
 }
 
+
 // Print Info
 void printDebugInfo() {
   char buff[50];
-  sprintf(buff, "\nListening at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
-  Serial.println(buff);
+  #if defined (__AVR_ATmega32U4__)
+    Serial.println("AVR ATmega 32U4");
+  #else
+    Serial.println("SAMD FEATHER M0");
+  #endif
+  #ifdef USING_RFM69_WING
+    Serial.println("Using RFM69 Wing: YES");
+  #else
+    Serial.println("Using RFM69 Wing: NO");
+  #endif
   Serial.print("RF69_SPI_CS: "); Serial.println(RF69_SPI_CS);
   Serial.print("RF69_IRQ_PIN: "); Serial.println(RF69_IRQ_PIN);
   Serial.print("RF69_IRQ_NUM: "); Serial.println(RF69_IRQ_NUM);
@@ -184,6 +228,8 @@ void printDebugInfo() {
   #else
     Serial.println("Encryption: Disabled");
   #endif
+  sprintf(buff, "\nListening at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  Serial.println(buff);
 }
 
 // Identify yourself
